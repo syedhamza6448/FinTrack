0dc84933c3939e43b658e28df6d857c129a7735e
Syed Hamza <164435480+syedhamza6448@users.noreply.github.com>
2026-02-24 19:31:18 +0500

    Transaction Page Created with 100% CRUD

---
+++ FinTrack/FinTrack/Models/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinTrack.Models.ViewModels
+{
+    public class TransactionViewModel
+    {
+        public List<Transaction> Transactions { get; set; } = new();
+        public List<Category> Categories { get; set; } = new();
+        public CreateTransactionViewModel NewTransaction { get; set; } = new();
+
+        public decimal TotalIncome {  get; set; }
+        public decimal TotalExpenses { get; set; }
+        public decimal NetBalance => TotalIncome - TotalExpenses;
+    }
+
+    public class CreateTransactionViewModel
+    {
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string Type { get; set; } = string.Empty;
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; } = DateTime.Today;
+
+        public string? Notes { get; set; }
+    }
+}
